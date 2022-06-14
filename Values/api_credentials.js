c5f941a6797d3fbdbd500fcb088521cf54f6aae3
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-06-14 16:37:53 +0500

    updated mongodb credentials

--- Values/api_credentials.js
+++ Values/api_credentials.js
@@ -1,5 +1,6 @@
 // const db_credentials="mongodb+srv://nzn_devops:DRhUdB715HO5qES6@cluster0.8kp3j.mongodb.net/icon_engine_db?retryWrites=true&w=majority"
-const db_credentials = "mongodb://localhost:27017/icon_engine_db";
+// const db_credentials = "mongodb://localhost:27017/icon_engine_db";
+const db_credentials = "mongodb+srv://zeeshan:zeeshan@sandbox.ry9p3.mongodb.net/icon_engine_db?retryWrites=true&w=majority"
 // const db_credentials =
 //   "mongodb+srv://nzndevops:oXIjtlsZCSEWZWbb@cluster0.lr92l.mongodb.net/icon_engine_db?retryWrites=true&w=majority"
   // const db_credentials =
